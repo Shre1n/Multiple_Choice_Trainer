b2e5d5e9ed35b79e7ef0bdc6abb5396ef55f3b0a
robin <robinhahn99@outlook.de>
2024-05-15 19:38:18 +0200

    init

---
+++ multiple-choice/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'multiple-choice',
+  webDir: 'www'
+};
+
+export default config;
