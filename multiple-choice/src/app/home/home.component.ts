1997f1c74418781daa6c5360c7e5a2b0d19dd653
robin <robinhahn99@outlook.de>
2024-05-28 12:11:21 +0200

    footer on all sites and footer icons navigate through pages

--- multiple-choice/src/app/home/home.component.ts
+++ multiple-choice/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {NavComponent} from "../nav/nav.component";
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   imports: [
+    IonicModule,
     NavComponent
   ],
   standalone: true
--- multiple-choice/src/app/nav/nav.component.ts
+++ multiple-choice/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {IonicModule} from "@ionic/angular";
+import {homeSharp, statsChartSharp, bookSharp, trophySharp}  from "ionicons/icons";
+import {addIcons} from "ionicons";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +15,30 @@ import {IonicModule} from "@ionic/angular";
 })
 export class NavComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    addIcons({homeSharp, statsChartSharp, bookSharp, trophySharp})
+  }
+
+
+  navToHome(){
+    this.router.navigate([''])
+  }
+
+  navToStatistik(){
+    this.router.navigate(['/statistik'])
+  }
+
+  navToCards(){
+    this.router.navigate(['/cards'])
+  }
+
+  navToAchivements(){
+    this.router.navigate(['/achivements'])
+  }
+
+
+
+
 
   ngOnInit() {}
 
