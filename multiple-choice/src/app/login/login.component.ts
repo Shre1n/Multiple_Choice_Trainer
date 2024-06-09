d4958fdf47f2e96496546be917dd71c998fab8b0
robin <robinhahn99@outlook.de>
2024-06-09 14:28:18 +0200

    login and register works now

--- multiple-choice/src/app/login/login.component.ts
+++ multiple-choice/src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../services/auth.service'
 import {Router} from "@angular/router";
 import {IonicModule} from "@ionic/angular";
 import {FormsModule} from "@angular/forms";
+import {ToastController} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-login',
@@ -19,8 +20,23 @@ export class LoginComponent{
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+  isToastOpen = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private toastController: ToastController) {}
+
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
+
+  async presentToast(position: 'middle') {
+    const toast = await this.toastController.create({
+      message: this.errorMessage,
+      duration: 5000,
+      position: position,
+    });
+
+    await toast.present();
+  }
 
   async login() {
     try {
@@ -28,8 +44,12 @@ export class LoginComponent{
       this.router.navigate(['/home']);
     } catch (error: unknown) {
       if (error instanceof Error) {
-        throw new Error(`Login failed: ${error.message}`);
+        this.errorMessage = `Login failed: No login found.`;
+      } else {
+        this.errorMessage = 'An unknown error occurred during login.';
       }
+      this.setOpen(true);
+      this.presentToast('middle');
     }
   }
 
