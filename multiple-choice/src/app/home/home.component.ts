beed115f28ea6d399b344b37bd6e94873f1a1332
robin <robinhahn99@outlook.de>
2024-06-16 12:51:34 +0200

    changes json format of modules

--- multiple-choice/src/app/home/home.component.ts
+++ multiple-choice/src/app/home/home.component.ts
@@ -4,6 +4,8 @@ import {IonicModule, NavController} from "@ionic/angular";
 import {addIcons} from "ionicons";
 import {personOutline,logOutOutline} from "ionicons/icons";
 import {AuthService} from "../services/auth.service";
+import {ModuleService} from "../services/module.service";
+import {ToastController} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-home',
@@ -17,16 +19,60 @@ import {AuthService} from "../services/auth.service";
 export class HomeComponent implements OnInit{
 
   isLoggedIn: boolean = false;
+  modules: any[] = [];
+  errorMessage: string = 'No Connection to External Server! 😢';
 
 
   constructor(private router: Router,
               public navCtrl: NavController,
-              private authService: AuthService,) {
+              private authService: AuthService,
+              private moduleService: ModuleService,
+              private toastController: ToastController) {
     addIcons({personOutline, logOutOutline});
+  }
+
+  async presentToast(position: 'middle') {
+    const toast = await this.toastController.create({
+      message: this.errorMessage,
+      duration: 10000,
+      position: position,
+    });
+
+    await toast.present();
   }
 
   ngOnInit() {
     this.checkLoginStatus();
+    this.loadModules();
+    this.checkForUpdates();
+  }
+
+  loadModules(): void {
+    this.moduleService.loadExternalModule().subscribe(
+      response => {
+        console.log('Modules loaded:', response);
+        this.modules = response;
+      },
+      error => {
+        console.error('Error loading modules:', error);
+        this.presentToast('middle');
+      }
+    );
+  }
+
+  checkForUpdates(): void {
+    this.moduleService.checkForUpdates().subscribe(
+      (response) => {
+        if (response.updatesAvailable) {
+          console.log('Updates available, reloading modules...');
+        } else {
+          console.log('No updates available');
+        }
+      },
+      (error) => {
+        console.error('Error checking for updates:', error);
+      }
+    );
   }
 
 
