746ffa4c62023ea925eccfec181f0a0fedd3348c
tomes <tom.washington.estermann@mni.thm.de>
2024-06-17 17:23:01 +0200

    added defalt Piechart

--- multiple-choice/src/app/statistik/statistik.component.ts
+++ multiple-choice/src/app/statistik/statistik.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IonicModule} from "@ionic/angular";
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-statistik',
@@ -10,9 +12,46 @@ import {IonicModule} from "@ionic/angular";
   ],
   standalone: true
 })
-export class StatistikComponent {
+export class StatistikComponent implements OnInit{
 
-  constructor() { }
+  public chart: any;
+
+  constructor() {
+    this.ngOnInit();
+  }
+
+  ngOnInit(): void {
+    this.createChart();
+  }
 
 
+  createChart(){
+
+    this.chart = new Chart("MyChart", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        labels: ['not started', 'Phase 1', 'Phase 2','Phase 3','Phase 4','Phase 5','Phase 6', ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [500, 300, 240, 100, 432, 253, 34],
+          backgroundColor: [
+            'pink',
+            'darkblue',
+            'lightblue',
+            'green',
+            'yellow',
+            'orange',
+            'blue',
+          ],
+          hoverOffset: 4
+        }],
+      },
+      options: {
+        aspectRatio:2.5
+      }
+
+    });
+  }
 }
+
