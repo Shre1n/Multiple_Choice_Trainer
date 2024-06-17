24397b4675455e695b905a6e3e93b3799b33d8cf
robin <robinhahn99@outlook.de>
2024-06-17 23:05:05 +0200

    merging sachen

--- multiple-choice/src/app/home/home.component.ts
+++ multiple-choice/src/app/home/home.component.ts
@@ -2,38 +2,124 @@ import {Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {GestureController, GestureDetail, IonicModule, NavController} from "@ionic/angular";
 import {addIcons} from "ionicons";
-import {personOutline,logOutOutline} from "ionicons/icons";
+import { personOutline, logOutOutline, calculatorOutline, schoolOutline, codeSlashOutline } from 'ionicons/icons';
 import {AuthService} from "../services/auth.service";
+import {ModuleService} from "../services/module.service";
+import {ToastController} from "@ionic/angular/standalone";
+import {NgStyle} from "@angular/common";
+import {ModuleModule} from "../module/module.module";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   imports: [
-    IonicModule
+    IonicModule,
+    NgStyle
   ],
   standalone: true
 })
 export class HomeComponent implements OnInit{
 
-  isLoggedIn!: boolean;
+  isLoggedIn!:boolean;
+  modules: any[] = [];
+  errorMessage: string = 'No Connection to External Server! :cry:';
+  categories: string[] = [];
 
 
   constructor(private router: Router,
               public navCtrl: NavController,
               private authService: AuthService,
-              private gestureCtrl: GestureController) {
-    addIcons({personOutline, logOutOutline});
+              private gestureCtrl: GestureController,
+              private moduleService: ModuleService,
+              private toastController: ToastController) {
+    addIcons({ personOutline, logOutOutline, calculatorOutline, schoolOutline, codeSlashOutline });
+
   }
 
   ngOnInit() {
     this.initializeSwipeGesture();
+    this.checkLoginStatus();
+    this.loadModules();
+    this.checkForUpdates();
+  }
+
+  async presentToast(position: 'middle') {
+    const toast = await this.toastController.create({
+      message: this.errorMessage,
+      duration: 10000,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  navSession (name:number) {
+    console.log(this.categories[name]);
+    this.router.navigate(['/session']);
+  }
+
+  checkForUpdates(): void {
+    this.moduleService.checkForUpdates().subscribe(
+      (response) => {
+        if (response.updatesAvailable) {
+          console.log('Updates available, reloading modules...');
+        } else {
+          console.log('No updates available');
+        }
+      },
+      (error) => {
+        console.error('Error checking for updates:', error);
+      }
+    );
+  }
+
+  async loadModules(){
+    this.moduleService.loadExternalModule().subscribe(
+      response => {
+        console.log('Modules loaded:', response);
+        this.modules = response;
+        this.extractCategories(response);
+      },
+      error => {
+        console.error('Error loading modules:', error);
+        this.presentToast('middle');
+      }
+    );
+  }
+
+  extractCategories(modules: any): void {
+    this.categories = Object.keys(modules).map(key => modules[key].category);
+  }
+
+  getCategoryIcon(category: string): string {
+    const icons: { [key: string]: string } = {
+      'Mathematics': 'calculator-outline',
+      'TypeScript': 'code-slash-outline',
+      // Füge hier weitere Kategorien und entsprechende Icons hinzu
+    };
+    return icons[category] || 'help-outline'; // Standardicon, wenn keine Kategorie übereinstimmt
+  }
+
+  getCategoryBackground(category: string): string {
+    const backgrounds: { [key: string]: string } = {
+      'Mathematics': 'var(--ion-color-primary)',
+      'TypeScript': 'var(--ion-color-info)',
+      // Füge hier weitere Kategorien und entsprechende Hintergründe hinzu
+    };
+    return backgrounds[category] || 'var(--ion-color-light)'; // Standardhintergrund, wenn keine Kategorie übereinstimmt
   }
 
 
   ionViewDidEnter(){
     this.authService.getCurrentUser()
     this.isLoggedIn = this.authService.isAuth();
+  }
+
+  checkLoginStatus() {
+    const user = this.authService.getCurrentUser();
+    this.isLoggedIn = user !== null;
+    console.log(this.isLoggedIn)
   }
 
   async logout() {
