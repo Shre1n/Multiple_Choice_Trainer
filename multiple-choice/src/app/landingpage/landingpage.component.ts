d7bff419d7f1bf7e43a5ce40936a03b2bc35e2df
Linda <fatlinda.islami@mni.thm.de>
2024-06-20 14:20:35 +0200

    Login und Register ausgelagert.

---
+++ multiple-choice/src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormsModule, NgForm} from "@angular/forms";
+import {IonicModule, NavController} from "@ionic/angular";
+
+@Component({
+  selector: 'app-landingpage',
+  templateUrl: './landingpage.component.html',
+  styleUrls: ['./landingpage.component.scss'],
+  imports: [
+    IonicModule,
+    FormsModule
+  ],
+  standalone: true
+})
+export class LandingpageComponent  implements OnInit {
+
+  constructor(  private router: Router,  )
+  { }
+
+  ngOnInit() {
+
+  }
+
+  openLoginComponente():void{
+    this.router.navigate(['/login']);
+  }
+  openRegisterComponente():void{
+    this.router.navigate(['/register']);
+  }
+}
